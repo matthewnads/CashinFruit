f0a6716b7eed4a5c8e7cff2a80bff306617a5a2a
Ahmed Jamal Jaffar <ajaffar4@uwo.ca>
2021-11-20 14:15:34 -0500

    twioli

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors= require('cors'); 
 const mongoose = require('mongoose');
 require('dotenv').config(); 
+const twilio = require('twilio');
+const accountSid = 'AC896a4d16930bd6c04578da0277f55303';
+const authToken = 'c292f6b938e29080c7be83df1d402c68';
+const client = new twilio(accountSid, authToken);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const app = express(); 
 const port = process.env.PORT||5000; 
@@ -21,4 +26,5 @@ app.use('', routes)
 
 app.listen(port, () => {
     console.log(`Server be running on port: ${port}`); 
+
 }); 
