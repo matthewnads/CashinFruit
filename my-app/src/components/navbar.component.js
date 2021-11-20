eb3fd3c2c52d569edf7e6b51757d33bd90ce7a97
Shanker Nadarajah <snadar5@uwo.ca>
2021-11-20 01:37:30 -0500

    more route changes

--- my-app/src/components/navbar.component.js
+++ my-app/src/components/navbar.component.js
@@ -10,9 +10,9 @@ export default class Navbar extends Component {
             
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
                 {/* check out bootstrap documentation for full details on classnames and stuff*/}
-                <Link to="/" className="navbar-brand">BudgetBud</Link>
+                <Link to="/" className="navbar-brand">CashinFruit</Link>
                 <div className="collpase navabar-collapse">
-                    <button type="button" className="btn btn-default navbar-btn bg-light">Other Projects</button>
+                 
                     
                 </div>
             </nav>
