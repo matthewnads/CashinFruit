a2a71dd317b275f32192f01f24bf5d638e2f7cbe
Shanker Nadarajah <snadar5@uwo.ca>
2021-11-20 01:39:23 -0500

    more route changes

--- my-app/src/components/linkchild.component.js
+++ my-app/src/components/linkchild.component.js
@@ -7,30 +7,9 @@ import axios from 'axios';
 export default class LinkChild extends Component {
     constructor(props) {
         super(props); 
-        this.getItem = this.getItem.bind(this); 
-        this.getBudget = this.getBudget.bind(this); 
-        this.getHook = this.getHook.bind(this); 
-        this.state={
-            budget:0
-        }
+
     }
 
-    getItem() {
-        axios.get('http://localhost:5000/link/api/get_item').then( res=> {
-            console.log(res.data.item)
-            }
-        )
-    }
-
-    getBudget(e) {
-        this.setState({
-            budget:e.target.value
-        })
-    }
-
-    getHook() {
-        axios.post('http://localhost:5000/link/api/test_hook')
-    }
     render() {
         
         return (
