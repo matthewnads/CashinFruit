cf95bed93c9bf6dc7dc1bb888ff8c4cc1bca098f
Shanker Nadarajah <snadar5@uwo.ca>
2021-11-19 23:14:50 -0500

    added some stuff to get plaid going

--- backend/routes.js
+++ backend/routes.js
@@ -13,36 +13,33 @@ const hook = 'https://6cff9ccdd81443604a8738b54d665a28.m.pipedream.net';
 
 
 
-// const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
-// const PLAID_SECRET = process.env.PLAID_SECRET;
-// const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
+const PLAID_CLIENT_ID= "5ff8b9f6f9c7ee001182a6a5"
+const PLAID_SECRET="c89bbab46049994073ab6ea9aee61e"
+const PLAID_ENV = 'sandbox';
 
-// const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || 'transactions').split(
-//     ',',
-//   );
+const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || 'transactions').split(
+    ',',
+  );
 
-// const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
-//     ',',
-// );
+const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || 'US').split(
+    ',',
+);
 
-// const PLAID_REDIRECT_URI = process.env.PLAID_REDIRECT_URI || '';
+const PLAID_REDIRECT_URI = process.env.PLAID_REDIRECT_URI || '';
 
 
-// let ACCESS_TOKEN = null;
-// let PUBLIC_TOKEN = null;
-// let ITEM_ID = null;
-// let linkToken = null; 
-// let currentUser = new User(); 
-
-// const client = new plaid.Client({
-//     clientID: PLAID_CLIENT_ID,
-//     secret: PLAID_SECRET,
-//     env: plaid.environments[PLAID_ENV],
-//   });
+let ACCESS_TOKEN = null;
+let PUBLIC_TOKEN = null;
+let ITEM_ID = null;
+let linkToken = null; 
+let currentUser = new User(); 
 
 
 router.route('/').get((req,res)=>{
     res.send("hello world")
 })
 
+router.route('/create').post((req,res)=> {
+//use this to create the link token 
+});
 module.exports = router;
